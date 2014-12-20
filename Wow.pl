d3d7cfe0085e17f9247f745e890ddd77397b1ae3
Karim bendouili <ccrashbandicot@hotmail.fr>
2014-12-20 22:31:06 +0100

    Wow Search Engine Dorker

---
+++ Wow.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+# Mr_Anarshi_T , Magnom , Ghosty , Leet Haxor
+#
+#   ************************************
+#    Dorker Wow.com (Enhanced by Google)
+#   ************************************
+
+use LWP::UserAgent;
+use URI::Escape;
+
+system $^O eq 'MSWin32' ? 'cls' : 'clear';
+
+print q {
+             *****************
+             | Wow Dorker    |
+             | CrashBandicot |
+             *****************
+
+ };
+
+ print "\n [+] Dork = ";
+ chomp($dorks = <STDIN>);
+
+for($i=1;$i<40;$i++) { 
+ $ua = LWP::UserAgent->new();
+ $ua->agent("Mozilla/5.0 (Windows; U; Windows NT 6.1) AppleWebKit/531.7.2 (KHTML, like Gecko) Version/5.1 Safari/531.7.2");
+ my $dork = uri_escape($dorks);
+ $req = $ua->get("http://www.wow.com/search?v_t=na&q=".$dork."&s_it=search-thp&page=".$i)->content;
+ while($req =~m/href=\"(.*?)\" property=\"f:title\"/g) {
+
+ 	print $1."\n";
+
+ 	open(SAVE,">>Wow.txt");
+ 	print SAVE "$1\n";
+ 	close(SAVE);
+ }
+
+}
