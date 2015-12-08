89950ff6d831e942d910625bedee9403a5eece05
Abdel bendouil <ccrashbandicot@hotmail.fr>
2015-12-08 02:05:50 +0100

    updated

--- Wow.pl
+++ Wow.pl
@@ -25,7 +25,8 @@ for($i=1;$i<40;$i++) {
  $ua = LWP::UserAgent->new();
  $ua->agent("Mozilla/5.0 (Windows; U; Windows NT 6.1) AppleWebKit/531.7.2 (KHTML, like Gecko) Version/5.1 Safari/531.7.2");
  my $dork = uri_escape($dorks);
- $req = $ua->get("http://www.wow.com/search?v_t=na&q=".$dork."&s_it=search-thp&page=".$i)->content;
+ $req = $ua->get("http://www.wow.com/search?v_t=na&page=".$i."&q=".$dork."&s_it=bottomsearchbox.nrf&oreq=f6bd7807ddc744188c9c06ea89a525a1")->content;
+
  while($req =~m/href=\"(.*?)\" property=\"f:title\"/g) {
 
  	print $1."\n";
