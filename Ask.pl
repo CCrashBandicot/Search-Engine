7563cf29b9ec837f5d1f69adf9c3eb7deb404457
Abdel bendouil <ccrashbandicot@hotmail.fr>
2015-12-08 02:01:53 +0100

    update

--- Ask.pl
+++ Ask.pl
@@ -26,7 +26,8 @@ for($i=1;$i<60;$i++) {
  $ua->agent("Mozilla/5.0 (Windows; U; Windows NT 6.1) AppleWebKit/531.7.2 (KHTML, like Gecko) Version/5.1 Safari/531.7.2");
  my $dork = uri_escape($dorks);
  $req = $ua->get("http://fr.ask.com/web?q=".$dork."&page=".$i)->content;
- while($req =~m/<span id=\"(.*?)\" class=\"txt3\" style=\"\">(.*?)<\/span>/g) {
+ while($req =~m/<span id=\"(.*?)\" class=\"txt3\">(.*?)<\/span>/g) # d
+  {
 
  	print "http://".$2."\n";
 
